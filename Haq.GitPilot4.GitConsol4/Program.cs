7c7e6e12d7b4e5c78e9fd1bb06a21ace3feda0b0
Asadul Haque <hoq_asad@yahoo.com>
2019-04-11 12:31:48 -0400

    Added new Console project in blank solution

---
+++ Haq.GitPilot4.GitConsol4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Haq.GitPilot4.GitConsol4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
